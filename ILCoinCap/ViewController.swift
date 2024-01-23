9c8ecaabed9eefe39570f7d1f201e057e504b044
Arina <arinapostnikova123@gmail.com>
2024-01-23 15:16:55 +0300

    Initial commit

---
+++ ILCoinCap/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  ILCoinCap
+//
+//  Created by Arina Postnikova on 23/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+}
